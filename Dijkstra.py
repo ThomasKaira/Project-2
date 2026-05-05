3c5fa59b843867ba3508aefadcac578f879bb7f9
Kenneth <56137407+ThomasKaira@users.noreply.github.com>
2026-05-04 20:43:10 -0600

    Add files via upload

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,6 +1,8 @@
 from collections import defaultdict 
 import sys
 import numpy as np
+import timeit
+import tracemalloc
 
 #-------------------------------------------------------------------------------
 #Data Structure Methods
@@ -88,10 +90,8 @@ def dijkstra_list(adj_list, src):
             new_path_dist = dist[cur_node] + weight
             if new_path_dist < dist[neighbor]:
                 dist[neighbor] = new_path_dist
-    
-    print(dist)
         
-    return None
+    return dist
 
 #For adjacency matrix, data supplied as a 2D matrix, each row-column pair being an edge.    
 def dijkstra_matrix(matrix, src):
@@ -116,17 +116,14 @@ def dijkstra_matrix(matrix, src):
                 new_dist = dist[u] + matrix[u][neighbor]
                 if new_dist < dist[neighbor]:
                     dist[neighbor] = new_dist
-                    
-    
-    print(dist)
-    
-    return None
+   
+    return dist
 
 
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
-#Adjacency List Creation
+#Graph Creation
 #-------------------------------------------------------------------------------
 edges1 = [('A', 'B', 4), ('A', 'C', 2), ('B', 'D', 5), ('C', 'D', 1), ('D', 'E', 3), ('E', 'F', 2)]
 list1 = create_adjacency_list(edges1)
@@ -147,24 +144,237 @@ edges4 = [('1', '2', 3), ('1', '3', 2), ('1', '4', 6), ('1', '5', 5), ('1', '6',
 list4 = create_adjacency_list(edges4)
 matrix4 = create_adjacency_matrix(edges4)
 
-matrix5 = generate_graph(10)
+matrix5 = generate_graph(100)
 list5 = matrix_to_list(matrix5)
-#print(matrix5)
-#print()
-#print(list5)
+
 #-------------------------------------------------------------------------------
 
-dijkstra_list(list1, 'A')
-dijkstra_list(list2, '1')
-dijkstra_list(list3, 'A')
-dijkstra_list(list4, '1')
-dijkstra_list(list5, '1')
+#-------------------------------------------------------------------------------
+#Algorithm Correctness Test
+#-------------------------------------------------------------------------------
 
-dijkstra_matrix(matrix1, 0)
-dijkstra_matrix(matrix1, 1)
-dijkstra_matrix(matrix1, 2)
+print("Results for Graph 1, all nodes, List version:")
+print(dijkstra_list(list1, 'A'))
+print(dijkstra_list(list1, 'B'))
+print(dijkstra_list(list1, 'C'))
+print(dijkstra_list(list1, 'D'))
+print(dijkstra_list(list1, 'E'))
+print(dijkstra_list(list1, 'F'))
+print()
 
-dijkstra_matrix(matrix2, 0)
-dijkstra_matrix(matrix3, 0)
-dijkstra_matrix(matrix4, 0)
-dijkstra_matrix(matrix5, 0)
+print("Results for Graph 1, all nodes, Matrix version:")
+print(dijkstra_matrix(matrix1, 0))
+print(dijkstra_matrix(matrix1, 1))
+print(dijkstra_matrix(matrix1, 2))
+print(dijkstra_matrix(matrix1, 3))
+print(dijkstra_matrix(matrix1, 4))
+print(dijkstra_matrix(matrix1, 5))
+print()
+
+print("Results for Graph 2, all nodes, List version:")
+print(dijkstra_list(list2, '1'))
+print(dijkstra_list(list2, '2'))
+print(dijkstra_list(list2, '3'))
+print(dijkstra_list(list2, '4'))
+print(dijkstra_list(list2, '5'))
+print(dijkstra_list(list2, '6'))
+print(dijkstra_list(list2, '7'))
+print()
+
+print("Results for Graph 2, all nodes, Matrix version:")
+print(dijkstra_matrix(matrix2, 0))
+print(dijkstra_matrix(matrix2, 1))
+print(dijkstra_matrix(matrix2, 2))
+print(dijkstra_matrix(matrix2, 3))
+print(dijkstra_matrix(matrix2, 4))
+print(dijkstra_matrix(matrix2, 5))
+print(dijkstra_matrix(matrix2, 6))
+print()
+
+print("Results for Graph 3, all nodes, List version:")
+print(dijkstra_list(list3, 'A'))
+print(dijkstra_list(list3, 'B'))
+print(dijkstra_list(list3, 'C'))
+print(dijkstra_list(list3, 'D'))
+print(dijkstra_list(list3, 'E'))
+print()
+
+print("Results for Graph 3, all nodes, Matrix version:")
+print(dijkstra_matrix(matrix3, 0))
+print(dijkstra_matrix(matrix3, 1))
+print(dijkstra_matrix(matrix3, 2))
+print(dijkstra_matrix(matrix3, 3))
+print(dijkstra_matrix(matrix3, 4))
+print()
+
+print("Results for Graph 4, all nodes, List version:")
+print(dijkstra_list(list4, '1'))
+print(dijkstra_list(list4, '2'))
+print(dijkstra_list(list4, '3'))
+print(dijkstra_list(list4, '4'))
+print(dijkstra_list(list4, '5'))
+print(dijkstra_list(list4, '6'))
+print()
+
+print("Results for Graph 4, all nodes, Matrix version:")
+print(dijkstra_matrix(matrix4, 0))
+print(dijkstra_matrix(matrix4, 1))
+print(dijkstra_matrix(matrix4, 2))
+print(dijkstra_matrix(matrix4, 3))
+print(dijkstra_matrix(matrix4, 4))
+print(dijkstra_matrix(matrix4, 5))
+print()
+
+print("Graph 5 is randomized and used solely for time and space analysis")
+print()
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+#Execution Time Trial
+#-------------------------------------------------------------------------------
+time1 = 0
+time2 = 0
+time3 = 0
+time4 = 0
+time5 = 0
+time6 = 0
+time7 = 0
+time8 = 0
+time9 = 0
+time10 = 0
+
+for i in range(5):
+    time1 += timeit.timeit("dijkstra_list(list1, 'A')", globals = globals(), number = 5000)
+
+for i in range(5):    
+    time2 += timeit.timeit("dijkstra_list(list2, '1')", globals = globals(), number = 5000)
+    
+for i in range(5):    
+    time3 += timeit.timeit("dijkstra_list(list3, 'A')", globals = globals(), number = 5000)
+    
+for i in range(5):    
+    time4 += timeit.timeit("dijkstra_list(list4, '1')", globals = globals(), number = 5000)
+    
+for i in range(5):    
+    time5 += timeit.timeit("dijkstra_list(list5, '1')", globals = globals(), number = 5000)
+
+for i in range(5):    
+    time6 += timeit.timeit("dijkstra_matrix(matrix1, 0)", globals = globals(), number = 5000)
+
+for i in range(5):    
+    time7 += timeit.timeit("dijkstra_matrix(matrix2, 0)", globals = globals(), number = 5000)
+    
+for i in range(5):    
+    time8 += timeit.timeit("dijkstra_matrix(matrix3, 0)", globals = globals(), number = 5000)
+    
+for i in range(5):    
+    time9 += timeit.timeit("dijkstra_matrix(matrix4, 0)", globals = globals(), number = 5000)
+    
+for i in range(5):    
+    time10 += timeit.timeit("dijkstra_matrix(matrix5, 0)", globals = globals(), number = 5000)
+    
+print(f"Time for List 1: {time1 * 1000} ms")
+print(f"Time for List 2: {time2 * 1000} ms")
+print(f"Time for List 3: {time3 * 1000} ms")
+print(f"Time for List 4: {time4 * 1000} ms")
+print(f"Time for List 5: {time5 * 1000} ms")
+print(f"Time for Matrix 1: {time6 * 1000} ms")
+print(f"Time for Matrix 2: {time7 * 1000} ms")
+print(f"Time for Matrix 3: {time8 * 1000} ms")
+print(f"Time for Matrix 4: {time9 * 1000} ms")
+print(f"Time for Matrix 5: {time10 * 1000} ms")
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+#Memory Usage Trial
+#-------------------------------------------------------------------------------
+
+tracemalloc.start()
+snapshot = tracemalloc.take_snapshot()
+temp = dijkstra_list(list1, 'A')
+snapshot = tracemalloc.take_snapshot()
+
+current, peak = tracemalloc.get_traced_memory()
+print(f"List 1 Memory Usage: {peak} bytes")
+tracemalloc.stop()
+
+tracemalloc.start()
+snapshot = tracemalloc.take_snapshot()
+temp = dijkstra_list(list2, '1')
+snapshot = tracemalloc.take_snapshot()
+
+current, peak = tracemalloc.get_traced_memory()
+print(f"List 2 Memory Usage: {peak} bytes")
+tracemalloc.stop()
+
+tracemalloc.start()
+snapshot = tracemalloc.take_snapshot()
+temp = dijkstra_list(list3, 'A')
+snapshot = tracemalloc.take_snapshot()
+
+current, peak = tracemalloc.get_traced_memory()
+print(f"List 3 Memory Usage: {peak} bytes")
+tracemalloc.stop()
+
+tracemalloc.start()
+snapshot = tracemalloc.take_snapshot()
+temp = dijkstra_list(list4, '1')
+snapshot = tracemalloc.take_snapshot()
+
+current, peak = tracemalloc.get_traced_memory()
+print(f"List 4 Memory Usage: {peak} bytes")
+tracemalloc.stop()
+
+tracemalloc.start()
+snapshot = tracemalloc.take_snapshot()
+temp = dijkstra_list(list5, '1')
+snapshot = tracemalloc.take_snapshot()
+
+current, peak = tracemalloc.get_traced_memory()
+print(f"List 5 Memory Usage: {peak} bytes")
+tracemalloc.stop()
+
+tracemalloc.start()
+snapshot = tracemalloc.take_snapshot()
+temp = dijkstra_matrix(matrix1, 0)
+snapshot = tracemalloc.take_snapshot()
+
+current, peak = tracemalloc.get_traced_memory()
+print(f"Matrix 1 Memory Usage: {peak} bytes")
+tracemalloc.stop()
+
+tracemalloc.start()
+snapshot = tracemalloc.take_snapshot()
+temp = dijkstra_matrix(matrix2, 0)
+snapshot = tracemalloc.take_snapshot()
+
+current, peak = tracemalloc.get_traced_memory()
+print(f"Matrix 2 Memory Usage: {peak} bytes")
+tracemalloc.stop()
+
+tracemalloc.start()
+snapshot = tracemalloc.take_snapshot()
+temp = dijkstra_matrix(matrix3, 0)
+snapshot = tracemalloc.take_snapshot()
+
+current, peak = tracemalloc.get_traced_memory()
+print(f"Matrix 3 Memory Usage: {peak} bytes")
+tracemalloc.stop()
+
+tracemalloc.start()
+snapshot = tracemalloc.take_snapshot()
+temp = dijkstra_matrix(matrix4, 0)
+snapshot = tracemalloc.take_snapshot()
+
+current, peak = tracemalloc.get_traced_memory()
+print(f"Matrix 4 Memory Usage: {peak} bytes")
+tracemalloc.stop()
+
+tracemalloc.start()
+snapshot = tracemalloc.take_snapshot()
+temp = dijkstra_matrix(matrix5, 0)
+snapshot = tracemalloc.take_snapshot()
+
+current, peak = tracemalloc.get_traced_memory()
+print(f"Matrix 5 Memory Usage: {peak} bytes")
+tracemalloc.stop()
